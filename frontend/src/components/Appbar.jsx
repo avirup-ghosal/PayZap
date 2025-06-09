ac0de88101f697a7c2e57cee90f28e059a865852
avirup-ghosal <avirupghosal2023@gmail.com>
2025-06-09 23:32:29 +0530

    updated app bar

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 export const Appbar = () => {
     return <div className="shadow h-14 flex justify-between">
         <div className="flex flex-col justify-center h-full ml-4">
@@ -9,7 +11,7 @@ export const Appbar = () => {
             </div>
             <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
                 <div className="flex flex-col justify-center h-full text-xl">
-                    U
+                    <Link to="/profile">Profile</Link>
                 </div>
             </div>
         </div>
